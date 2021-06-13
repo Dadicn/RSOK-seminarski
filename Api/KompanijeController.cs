c407752d9e84042b40b3483aff509c4337ce1c67
Dadicn <dzoniczbg99@gmail.com>
2021-06-14 01:25:11 +0200

    Dodaj API kontrole

---
+++ Api/KompanijeController.cs
@@ -0,0 +1,43 @@
+﻿using EvidencijaAvioKarata.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EvidencijaAvioKarata.Controllers.Api
+{
+    public class KompanijeController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public KompanijeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        public IEnumerable<Kompanija> DajSveKompanije()
+        {
+            var kompanijeUBazi = _context.Kompanije.Include(p => p.Klasa).ToList();
+
+            return kompanijeUBazi;
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Obrisi(int id)
+        {
+            var kompanijaUBazi = _context.Kompanije.SingleOrDefault(p => p.Id == id);
+
+            if (kompanijaUBazi == null)
+                return NotFound();
+
+            _context.Kompanije.Remove(kompanijaUBazi);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Api/KupciController.cs
@@ -0,0 +1,43 @@
+﻿using EvidencijaAvioKarata.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EvidencijaAvioKarata.Controllers.Api
+{
+    public class KupciController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public KupciController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: /api/kupci
+        [HttpGet]
+        public IEnumerable<Kupac> DajSveKupce()
+        {
+            var kupciUBazi = _context.Kupci.Include(c => c.Karta).ToList();
+
+            return kupciUBazi;
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Obrisi(int id)
+        {
+            var kupacUBazi = _context.Kupci.SingleOrDefault(p => p.Id == id);
+
+            if (kupacUBazi == null)
+                return NotFound();
+
+            _context.Kupci.Remove(kupacUBazi);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
