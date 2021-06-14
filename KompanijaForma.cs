dd9a1172957fe926c436f9496d0622d0f69d747f
FirstClassIdiot <vlaovic.milica@gmail.com>
2021-06-14 13:03:39 +0200

    Dodaj view modele

---
+++ KompanijaForma.cs
@@ -0,0 +1,14 @@
+﻿using EvidencijaAvioKarata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvidencijaAvioKarata.ViewModels
+{
+    public class KompanijaForma
+    {
+        public Kompanija Kompanija { get; set; }
+        public IEnumerable<Klasa> Klase { get; set; }
+    }
+}
