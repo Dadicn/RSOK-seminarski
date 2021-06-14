04809bc9f4501d3f94562ffad4e30292cc15eb94
FirstClassIdiot <vlaovic.milica@gmail.com>
2021-06-14 13:02:14 +0200

    Dodaj view modele

---
+++ KartaForma.cs
@@ -0,0 +1,15 @@
+﻿using EvidencijaAvioKarata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvidencijaAvioKarata.ViewModels
+{
+    public class KartaForma
+    {
+        public Karta Karta { get; set; }
+        public IEnumerable<Destinacija> Destinacije { get; set; }
+        public IEnumerable<Kompanija> Kompanije { get; set; }
+    }
+}
